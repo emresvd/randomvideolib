deee22a7ca6f3d57376ca2ce72efc5e4197ce8fe
emresvd <emresevindi99@gmail.com>
2023-01-06 01:06:45 +0300

    sssssss

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+
+if sys.argv[-1] == "main.py":
+    import randomyoutubevideo
+    print(randomyoutubevideo.get_random_video_id())
+
+if sys.argv[-1] == "clean":
+    import shutil
+    for i in ["build", "dist", "urlgenlib.egg-info"]:
+        shutil.rmtree(i)
+
+if sys.argv[-1] == "build":
+    import os
+    os.system("python setup.py sdist bdist_wheel")
+
+if sys.argv[-1] == "upload":
+    import os
+    os.system("twine upload dist/*")
